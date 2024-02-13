340664c4ec9fac3d42cb65b2fab5ecad1842d0aa
Rafael Miranda <98825246+Yiizo@users.noreply.github.com>
2024-02-13 12:32:47 +0000

    Update setup.sh

--- Labs/05/setup.sh
+++ Labs/05/setup.sh
@@ -6,11 +6,11 @@ suffix=${guid//[-]/}
 suffix=${suffix:0:18}
 
 # Set the necessary variables
-RESOURCE_GROUP="rg-dp100-l${suffix}"
+RESOURCE_GROUP="rg-dp3007-l${suffix}"
 RESOURCE_PROVIDER="Microsoft.MachineLearning"
 REGIONS=("eastus" "westus" "centralus" "northeurope" "westeurope")
 RANDOM_REGION=${REGIONS[$RANDOM % ${#REGIONS[@]}]}
-WORKSPACE_NAME="mlw-dp100-l${suffix}"
+WORKSPACE_NAME="mlw-dp3007-l${suffix}"
 COMPUTE_CLUSTER="aml-cluster"
 
 # Register the Azure Machine Learning resource provider in the subscription
@@ -28,7 +28,7 @@ az configure --defaults workspace=$WORKSPACE_NAME
 
 # Create compute cluster
 echo "Creating a compute cluster with name: " $COMPUTE_CLUSTER
-az ml compute create --name ${COMPUTE_CLUSTER} --size STANDARD_DS11_V2 --max-instances 2 --type AmlCompute 
+az ml compute create --name ${COMPUTE_CLUSTER} --size STANDARD_DS2_V2 --max-instances 2 --type AmlCompute 
 
 # Create data asset
 echo "Creating a data asset with name: diabetes-folder"
